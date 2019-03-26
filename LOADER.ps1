fca072542865d762cabdc5057f7cc9e5b6c764d2
Oliver Smith <oliver@kfs.org>
2019-03-26 10:33:56 -0700

    Fixed path resolution in LOADER

--- LOADER.ps1
+++ LOADER.ps1
@@ -1,8 +1,10 @@
 [CmdletBinding(SupportsShouldProcess = $true)]
 Param([String] $ScriptPath=$PSScriptRoot)
 
-Get-ChildItem $PSScriptRoot -File -Filter *.rc.ps1 | ForEach {
-	if ($pscmdlet.ShouldProcess($_.FullName, "Source")) {
-		. $_.FullName
+$pscmdlet.WriteVerbose("Searching $ScriptPath for .rc.ps1 scripts")
+Get-ChildItem $ScriptPath -File -Filter *.rc.ps1 | ForEach {
+  $script = $_.FullName
+	if ($pscmdlet.ShouldProcess($script, "Source")) {
+		. $script
 	}
 }
